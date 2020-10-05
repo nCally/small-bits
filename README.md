440f2313d33143a23e132bc7a6c7bc448a71a408
nCally <cartrf@live.com>
2020-10-05 12:07:00 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+### Small bits
+These are small bits of code I find to be helpful for me in different repetitive task I seem to do often.
---
+++ nginx-server-block/sbscript.py
@@ -0,0 +1,90 @@
+import os
+import pwd
+import grp
+import sys
+import shutil
+
+basedir = "/var/www/"
+sites_available_dir = "/etc/nginx/sites-available/"
+
+domain_name = input("domain name: ")
+
+mkdir = basedir + domain_name + "/html"
+
+try:
+    # 1.
+    # make a new directory in /var/www
+    os.makedirs(mkdir)
+
+    # 2.
+    # assign ownership of the directory with the $USER environment variable
+    user = os.getenv("USER")
+    os.chown(mkdir, pwd.getpwnam(user).pw_uid, grp.getgrnam(user).gr_gid)
+
+    # 3.
+    # add permission
+    os.chmod(basedir + domain_name, 0o755)  # add 0o755 for permission
+
+    # 4.
+    # copy the index.html from this current directory to the /var/www/XX/html newly created
+    src = os.getcwd() + "/index.html"
+    shutil.copy2(src, mkdir)
+
+    # 5.
+    # choose which type of server file to create; whether for react build or node server
+    choice = input("setup -> [1] react [2] node api: ")
+
+    try:
+        # 6
+        # create site-available file
+
+        server_file = open(sites_available_dir + "/" + domain_name, "x")
+        port = ''  # if there is need for it, in the case of a node app
+
+# 7
+# choose the template file to use to create the server file
+        if choice == "1":
+            template = os.getcwd() + "/react.txt"
+
+        elif choice == "2":
+            template = os.getcwd() + "/node.txt"
+
+# 7a
+            # if the choice is 2, meaning its a node app and running on a port
+            # so we get the input of the port the app is running on
+            port = input("what port is the app running on?: ")
+
+# 8
+# open the template that was choosen in step 7
+        template_file = open(template, "rt")
+
+# 9
+# map through the lines in the template file to replace the domain name and port number
+        for line in template_file:
+            server_file.write(line.replace(
+                '$DOMAIN', domain_name).replace('$PORT', port))
+
+# 10
+# close the server file and template file
+        server_file.close()
+        template_file.close()
+
+# 11
+# link the site available file and the site enabled nginx
+        os.system('sudo ln -s /etc/nginx/sites-available/' +
+                  domain_name + " /etc/nginx/sites-enabled/")
+
+# 12
+# test the linking and other settings
+        os.system('sudo nginx -t')
+
+# 13
+# restart nginx
+        os.system('sudo systemctl restart nginx')
+
+    except FileExistsError:
+        print("This file already exists")
+
+
+except OSError:
+    print("failed to make directory")
